a08ef1869e693338353272c7bb89c4a21a18b3dd
Adarsh Kumar <44334235+4d4rshkumar@users.noreply.github.com>
2020-08-30 16:08:00 +0530

    Add files via upload

---
+++ asignments/uniquePairs.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int uniquePaths(int m, int n)
+{
+    if (m <= 0 || n <= 0)
+        return 0;
+    long long res = 1;
+    for (int i = n; i < m + n - 1; i++)
+    {
+        res = res * i / (i - n + 1);
+    }
+    return (int)res;
+}
+int main(){
+    cout<<uniquePaths(4,3);
+}
